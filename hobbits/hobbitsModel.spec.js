9ed8047525920f6717bc1aff2ceda2df084108dc
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-19 14:35:48 -0500

    uudate testing

--- hobbits/hobbitsModel.spec.js
+++ hobbits/hobbitsModel.spec.js
@@ -3,12 +3,22 @@ const db = require('../data/dbConfig.js');
 
 
 describe('hobbits model', () => {
+    beforeEach(async () => {
+        await db('hobbits').truncate();
+      });
+
     it('should set evironment to testing', () => {
         expect(process.env.DB_ENV).toBe('testing');
     });
     
     describe('insert()', () => {
-    
+        it('should insert hobbits into db', async () => {
+           await Hobbits.insert({ name: 'Gaffer' });
+            await Hobbits.insert({ name: 'Frodo' });
+
+            let hobbits = await db('hobbits');
+            expect(hobbits).toHaveLength(2);
+        })
 
         it('should insert hobbits into db', async () => {
             const [id] = await Hobbits.insert({ name: "Gaffer"});
